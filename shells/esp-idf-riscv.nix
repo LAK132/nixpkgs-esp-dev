8ec0d5b79172c432880f33f8678373bc33cd09d9
HTunne <hazzatun@gmail.com>
2026-04-11 22:43:07 +0100

    Remove depricated shells.

--- shells/esp-idf-riscv.nix
+++ shells/esp-idf-riscv.nix
@@ -10,14 +10,10 @@
   },
 }:
 
-builtins.warn
-  "[DEPRECATION WARNING] Chip specific shell will be removed starting ESP-IDF 6.0. Use esp-idf-full instead."
+pkgs.mkShell {
+  name = "esp-idf-riscv-shell";
 
-  pkgs.mkShell
-  {
-    name = "esp-idf-esp32-shell";
-
-    buildInputs = with pkgs; [
-      esp-idf-esp32
-    ];
-  }
+  buildInputs = with pkgs; [
+    esp-idf-riscv
+  ];
+}
